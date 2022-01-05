6fff463b8b5bbdc144df1b7741ae40edb8fd82b3
Steven Richart <steven.richart@laposte.net>
2022-01-05 15:20:35 +0100

    verification du formulaire basique

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -28,3 +28,62 @@ function launchModal() {
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+function validate ()                             
+{ 
+//form var
+var first = document.forms["reserve"]["first"];
+var last = document.forms["reserve"]["last"];              
+var email = document.forms["reserve"]["email"]; 
+var quantity = document.forms["reserve"]["quantity"];
+var location1 = document.forms["reserve"]["location1"];
+var location2 = document.forms["reserve"]["location2"];
+var location3 = document.forms["reserve"]["location3"];
+var location4 = document.forms["reserve"]["location4"];
+var location5 = document.forms["reserve"]["location5"];
+var location6 = document.forms["reserve"]["location6"];
+
+//form verif
+if (first.value == "")                                  
+    { 
+        alert("Mettez votre Prénom."); 
+        first.focus(); 
+        return false; 
+    }
+    if (last.value == "")                                  
+    { 
+        alert("Mettez votre nom."); 
+        last.focus(); 
+        return false; 
+    }    
+    if (email.value == "")                                   
+    { 
+        alert("Mettez une adresse email valide."); 
+        email.focus(); 
+        return false; 
+    }    
+    if (email.value.indexOf("@", 0) < 0)                 
+    { 
+        alert("Mettez une adresse email valide."); 
+        email.focus(); 
+        return false; 
+    }    
+    if (email.value.indexOf(".", 0) < 0)                 
+    { 
+        alert("Mettez une adresse email valide."); 
+        email.focus(); 
+        return false; 
+    }     
+    if (quantity.value == "")                                  
+    { 
+        alert("Mettez un nombre valide."); 
+        quantity.focus(); 
+        return false; 
+    }
+    if (!location1.checked && !location2.checked && !location3.checked && !location4.checked && !location5.checked && !location6.checked)                                  
+    { 
+        alert("Veuillez choisir une ville."); 
+        return false; 
+    }
+    return true;
+}
